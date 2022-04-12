be1ced56077f2d97782b589633a1a6a468458290
Devraaj <102584162+Devraaj-Dey-Talukdar@users.noreply.github.com>
2022-04-12 16:07:50 +0530

    Functions - 1

---
+++ 01_functions.js
@@ -0,0 +1,15 @@
+function sayHello(name) {
+  console.log(`Hello there ${name} , How are you?`);
+}
+
+// sayHello("Devraaj");
+// sayHello("SOurav");
+
+function namastey(){
+    return "Hello in India";//return statement is used to return the value from the function
+}
+
+var greetings = namastey();
+console.log(greetings); // Hello in India
+
+console.log(namastey());  // function calling
