def4dabe4d536eae5c368bb02d824038a78e91e8
Devraaj <102584162+Devraaj-Dey-Talukdar@users.noreply.github.com>
2022-04-12 14:06:22 +0530

    Conditionals 01

---
+++ 04_conditionals.js
@@ -0,0 +1,17 @@
+var temperature;
+
+temperature = 15;
+
+//TODO : Go to google and get the data 
+
+if(temperature <= 20){ //These evaluates to boolean values i.e. true or false
+    console.log("It is cold outside")
+}
+
+else if(temperature <= 30){
+     console.log("It is moderate outside ;)")
+}
+
+else{
+    console.log("It's too hot outside")
+}
