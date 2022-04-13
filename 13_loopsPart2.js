524d69d1cc533e1d971e7a6e3f33ce28056ff4cf
Devraaj <102584162+Devraaj-Dey-Talukdar@users.noreply.github.com>
2022-04-13 19:24:48 +0530

    loop - 2

---
+++ 13_loopsPart2.js
@@ -0,0 +1,11 @@
+const myState = ["West-Bengal",
+    "NewYork",
+    "California",
+    "Bangalore",
+    1947,
+    "Texas",
+    "Kolkata"];
+
+    myState.forEach((s) => console.log(s));
+    //forEach is used to iterate over the array.
+    //forEach is helpful when we want to perform some operation on each element of the array.
